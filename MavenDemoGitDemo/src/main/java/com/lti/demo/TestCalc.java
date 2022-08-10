faf96485ee7aa39b428ff8ccd30864029c96f6bd
neeraj2000-kt <neerajkumarkt01@gmail.com>
2022-08-10 12:47:29 +0530

    Second commit with changes

---
+++ MavenDemoGitDemo/src/main/java/com/lti/demo/TestCalc.java
@@ -0,0 +1,13 @@
+package com.lti.demo;
+
+public class TestCalc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator c = new Calculator();
+		c.sayHello();
+		c.addNos(3, 1);
+		c.subNos(3, 1);
+	}
+
+}
